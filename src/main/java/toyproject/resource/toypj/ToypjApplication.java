24e1045692d92bf9fb6af200bf21b13ec0f45d00
chocozzang <jangjh_05@naver.com>
2024-01-02 21:43:23 +0900

    240102 commit

---
+++ src/main/java/toyproject/resource/toypj/ToypjApplication.java
@@ -0,0 +1,13 @@
+package toyproject.resource.toypj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToypjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToypjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toyproject/resource/toypj/ToypjApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.resource.toypj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToypjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
