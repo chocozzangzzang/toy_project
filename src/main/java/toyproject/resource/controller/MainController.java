c384d09c128478d8b380e39150e7998d942559d9
chocozzang <jangjh_05@naver.com>
2024-01-06 22:07:22 +0900

    240106 JWT 세션 구현

--- src/main/java/toyproject/resource/controller/MainController.java
+++ src/main/java/toyproject/resource/controller/MainController.java
@@ -1,9 +1,15 @@
 package toyproject.resource.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
@@ -11,7 +17,21 @@ public class MainController {
 
     @GetMapping("/")
     public String main() {
-        return "mainController";
+
+        // JWT는 일시적으로 세션을 생성해줌....
+
+        // id
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        // role - iterator로 뽑아내야함
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();;
+        String role = auth.getAuthority();
+
+        return "mainController" + " " + username + " " + role;
     }
 
 }
